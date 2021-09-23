54bf3c42ef6524de23ebc50881b14fcfca470829
qqgameover <kasper35@live.no>
2021-09-24 00:17:22 +0200

    init, forgot to update xd

---
+++ shoppingcart/src/app/component/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  public productsList: any;
+  constructor(private api: ApiService, private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.api.getProducts()
+      .subscribe(res => {
+        this.productsList = res;
+
+        this.productsList.forEach((e: any) => {
+          Object.assign(e, { quantity: 1, total: e.price })
+        })
+      })
+  }
+  addToCart(item: any) {
+    this.cartService.addToCart(item)
+  }
+
+
+}
---
+++ shoppingcart/src/app/service/cart.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  public cartItemList: any = [];
+  public productList = new BehaviorSubject<any>([]);
+
+  constructor() { }
+
+  getProducts() {
+    return this.productList.asObservable();
+  }
+
+  setProduct(product: any) {
+    this.cartItemList.push(...product);
+    this.productList.next(product);
+  }
+
+  addToCart(product: any) {
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+    console.log(this.cartItemList)
+  }
+  getTotalPrice(): number {
+    let grandTotal = 0;
+    this.cartItemList.map((e: any) => {
+      grandTotal += e.total;
+    })
+    return grandTotal;
+  }
+  removeCartItem(product: any) {
+    this.cartItemList.map((e: any, index: number) => {
+      if (product.id == e.id) {
+        this.cartItemList.splice(index, 1);
+      }
+    })
+  }
+  removeAll() {
+    this.cartItemList = [];
+    this.productList.next(this.cartItemList);
+  }
+}
